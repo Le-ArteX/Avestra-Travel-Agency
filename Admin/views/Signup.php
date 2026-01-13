9f72dd808da53b48f21362a362c9e7d125b99dfa
Mursalin Leon <mursalinleon92@gmail.com>
2026-01-13 12:28:49 +0600

    updated

--- Admin/views/Signup.php
+++ Admin/views/Signup.php
@@ -16,15 +16,36 @@ include '../controller/SignupFormDataHandler.php';
 <body>
     <div class="signup-container">
         <h2>Create Your Account</h2>
-        <?php if (!empty($general_error)): ?>
-            <div class="general-error-message"><?php echo $general_error; ?></div>
-        <?php endif; ?>
-        <?php if (!empty($_SESSION['success_message'])): ?>
-            <div class="success-message"><?php echo $_SESSION['success_message']; unset($_SESSION['success_message']); ?></div>
-        <?php endif; ?>
-        <?php if (!empty($_SESSION['success_error'])): ?>
-            <div class="general-error-message"><?php echo $_SESSION['success_error']; unset($_SESSION['success_error']); ?></div>
-        <?php endif; ?>
+        <?php
+        $messages = [];
+
+        // Collect messages and their types
+        if (!empty($general_error)) {
+            $messages[] = ['type' => 'general-error-message', 'text' => $general_error];
+        }
+        if (!empty($_SESSION['success_message'])) {
+            $messages[] = ['type' => 'success-message', 'text' => $_SESSION['success_message']];
+            unset($_SESSION['success_message']);
+        }
+        if (!empty($_SESSION['success_error'])) {
+            $messages[] = ['type' => 'general-error-message', 'text' => $_SESSION['success_error']];
+            unset($_SESSION['success_error']);
+        }
+        // Add more message types as needed
+        if (!empty($_SESSION['info_message'])) {
+            $messages[] = ['type' => 'info-message', 'text' => $_SESSION['info_message']];
+            unset($_SESSION['info_message']);
+        }
+        if (!empty($_SESSION['warning_message'])) {
+            $messages[] = ['type' => 'warning-message', 'text' => $_SESSION['warning_message']];
+            unset($_SESSION['warning_message']);
+        }
+
+        // Display all messages
+        foreach ($messages as $msg) {
+            echo '<div class="' . htmlspecialchars($msg['type']) . '">' . htmlspecialchars($msg['text']) . '</div>';
+        }
+        ?>
         
         <form class="signup-form" action="../controller/SignupValidation.php" method="post">
             <div class="form-group">
